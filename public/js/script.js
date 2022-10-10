b638ce7d5dadeac5627b4dde720a409d3d5ff5d3
MakeithappenMike <omichaelaaron@gmail.com>
2022-10-09 23:26:16 -0400

    linking working

--- public/js/script.js
+++ public/js/script.js
@@ -1,5 +1,34 @@
+// Handle login
+var loginButton = $('.loginButton');
+loginButton.on('click', function() {
+  window.location.replace("/public/browse.html");
+})
+
+// Handle logout
+var logOutButton = $('.logOutButton');
+logOutButton.on('click', function() {
+  window.location.replace("/public/index.html");
+})
+
+// Handle library
+var libraryTab = $('.yourLibraryButton');
+libraryTab.on('click', function() {
+  window.location.replace("/public/library.html");
+})
+
+// Handle browse
+var browseTab = $('.browseButton');
+browseTab.on('click', function() {
+  window.location.replace("/public/browse.html");
+})
+
 // Handle drop down toggle
 var readStatus = $('.dropdown');
 readStatus.on('click', function() {
   $(".dropdown").addClass( "is-active");
 })
+
+// Handle dropdown coloring
+if (readStatus.val() === "You have not read this book.") {
+    $(".dropdown").css("background-color", "red");
+}
