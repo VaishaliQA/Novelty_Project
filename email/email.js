d5144034214ae62212be145164b8bb1fd9da87d9
jon-shogren <jon.shogren@mailchimp.com>
2022-10-11 08:27:47 -0400

    written as a function with static data

--- email/email.js
+++ email/email.js
@@ -2,16 +2,19 @@
 // https://github.com/sendgrid/sendgrid-nodejs
 
 const sgMail = require('@sendgrid/mail')
+
+function sendMessage(name, bookUrl, confirmUrl) {
+
 sgMail.setApiKey(process.env.SENDGRID_API_KEY)
 const msg = {
   template_id: "d-e8b4d3e225d644e6ad26a1c7f1c73f3e",
-  to: 'jonshogren@mac.com', // your recipient
+  to: 'michael_martens@intuit.com', // your recipient
   from: 'jon@fart.institute', // your verified sender
   subject: 'Sending with SendGrid is Fun',
   dynamic_template_data: {
-    "name": "Jonathan",
-    "bookUrl": "https://m.media-amazon.com/images/I/51F0uan3+EL._AC_SY780_.jpg",
-    "confirm": "https://www.mandrill.fun"
+    "name": name,
+    "bookUrl": bookUrl,
+    "confirm": confirmUrl
   }
 }
 sgMail
@@ -22,3 +25,6 @@ sgMail
   .catch((error) => {
     console.error(error)
   })
+}
+
+sendMessage("Michael", "https://m.media-amazon.com/images/I/51F0uan3+EL._AC_SY780_.jpg", "https://www.mandrill.fun")
