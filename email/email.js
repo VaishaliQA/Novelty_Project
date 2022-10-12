96d9752c1697f5f065177132ddcd3e64487b4bbf
jon-shogren <jon.shogren@mailchimp.com>
2022-10-12 18:02:30 -0400

    formatting changes

--- email/email.js
+++ email/email.js
@@ -3,28 +3,28 @@
 
 const sgMail = require('@sendgrid/mail')
 
-function sendMessage(name, bookUrl, confirmUrl) {
+export function sendMessage(recipient, name, bookUrl, confirmUrl) {
 
-sgMail.setApiKey(process.env.SENDGRID_API_KEY)
-const msg = {
-  template_id: "d-e8b4d3e225d644e6ad26a1c7f1c73f3e",
-  to: 'michael_martens@intuit.com', // your recipient
-  from: 'jon@fart.institute', // your verified sender
-  subject: 'Sending with SendGrid is Fun',
-  dynamic_template_data: {
-    "name": name,
-    "bookUrl": bookUrl,
-    "confirm": confirmUrl
-  }
-}
-sgMail
-  .send(msg)
-  .then(() => {
-    console.log('Email sent!')
-  })
-  .catch((error) => {
-    console.error(error)
-  })
+    sgMail.setApiKey(process.env.SENDGRID_API_KEY)
+    const msg = {
+    template_id: "d-e8b4d3e225d644e6ad26a1c7f1c73f3e",
+    to: recipient, // your recipient
+    from: 'jon@fart.institute', // your verified sender
+    subject: 'Sending with SendGrid is Fun',
+    dynamic_template_data: {
+        "name": name,
+        "bookUrl": bookUrl,
+        "confirm": confirmUrl
+    }
+    }
+    sgMail
+    .send(msg)
+    .then(() => {
+        console.log('Email sent!')
+    })
+    .catch((error) => {
+        console.error(error)
+    })
 }
 
-sendMessage("Michael", "https://m.media-amazon.com/images/I/51F0uan3+EL._AC_SY780_.jpg", "https://www.mandrill.fun")
+sendMessage("jonshogren@fart.institute", "Jon", "https://m.media-amazon.com/images/I/51F0uan3+EL._AC_SY780_.jpg", "https://www.mandrill.fun")
