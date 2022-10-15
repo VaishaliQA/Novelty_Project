3e808a6c69bcc7c9a8c88093732384d672102dc6
MakeithappenMike <omichaelaaron@gmail.com>
2022-10-15 18:43:18 -0400

    resetn main handlebars

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -2,24 +2,31 @@ const router = require("express").Router();
 const { User, Book } = require("../models");
 const withAuth = require("../utils/auth");
 
-// find all users and map data.
-router.get("/", withAuth, async (req, res) => {
-  try {
-    const userData = await User.findAll({
-      attributes: { exclude: ["password"] },
-      order: [["first_name", "ASC"]],
-    });
-
-    const users = userData.map((project) => project.get({ plain: true }));
-
-    res.render("browsepage", {
-      users,
-      logged_in: req.session.logged_in,
-    });
-  } catch (err) {
-    res.status(500).json(err);
-  }
+// TODO: Remove this for testing and uncomment the get router below
+// GET all users
+// localhost:3001/api/users/
+router.get('/', async (req, res) => {
+  res.render('librarypage');
 });
+
+// // find all users and map data.
+// router.get("/", withAuth, async (req, res) => {
+//   try {
+//     const userData = await User.findAll({
+//       attributes: { exclude: ["password"] },
+//       order: [["first_name", "ASC"]],
+//     });
+
+//     const users = userData.map((project) => project.get({ plain: true }));
+
+//     res.render("browsepage", {
+//       users,
+//       logged_in: req.session.logged_in,
+//     });
+//   } catch (err) {
+//     res.status(500).json(err);
+//   }
+// });
 
 // GET a book by ID
 router.get('/:id', async (req, res) => {
