b16f11243668a3eadeeb1ba279b56573b93630ef
VaishaliQA <patelvaishali1890@gmail.com>
2022-10-16 10:56:16 -0700

    Added code for open book modlal and api call

---
+++ public/js/browse.js
@@ -0,0 +1,26 @@
+// This method is called when user click on login button
+const openDialog = async (event) => {
+  event.preventDefault();
+
+  // Open book modal
+  openBrowseModal();
+  const response = await fetch("/api/books/1", {
+    method: "GET",
+  });
+
+  const bookRes = await response.json();
+
+  document.querySelector("#booktitle").textContent = "Title: " + bookRes.title;
+  document.querySelector("#bookdesc").textContent =
+    "Description: " + bookRes.description;
+  document.querySelector("#bookauthors").textContent =
+    "Authors: " + bookRes.authors.replace("[ '", "").replace("' ]", "");
+  document.querySelector("#bookcategories").textContent =
+    "Categories: " + bookRes.categories.replace("[ '", "").replace("' ]", "");
+  document.querySelector("#ownedby").textContent =
+    "Owned By: " + bookRes.owner.first_name + " " + bookRes.owner.last_name;
+  document.querySelector("#bookstatus").textContent =
+    "Status: " + bookRes.available;
+};
+
+document.querySelector("#step1").addEventListener("click", openDialog);
