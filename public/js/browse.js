11c547fe44d2ba438fda415d9a4968ad4af08dea
jmg <macgreene14@gmail.com>
2022-10-16 22:12:54 -0400

    updated browse view to render books from db, popup modal on click

---
+++ public/js/browse.js
@@ -0,0 +1,6 @@
+const libraryButton = document.querySelector("#libraryButton");
+
+// Redirect to librarypage
+libraryButton.addEventListener("click", () => {
+  document.location.replace("/library");
+});
