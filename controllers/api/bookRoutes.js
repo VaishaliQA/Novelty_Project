016f2fbf554875b1d7ba5a3c9c455782a079a126
VaishaliQA <patelvaishali1890@gmail.com>
2022-10-17 16:46:57 -0700

    Added Put and post routes for creating new book and update status of book

--- controllers/api/bookRoutes.js
+++ controllers/api/bookRoutes.js
@@ -78,13 +78,41 @@ router.get("/:id", async (req, res) => {
   }
 });
 
-// POST new book
+// POST new book (Add new book in database)
 // localhost:3001/api/books/
-router.post("/", async (req, res) => {});
+router.post("/", async (req, res) => {
+  try {
+    const postData = await Book.create({
+      title: req.body.title,
+      isbn13: req.body.isbn13,
+      authors: req.body.authors,
+      published_date: req.body.published_date,
+      description: req.body.description,
+      categories: req.body.categories,
+      thumbnail_url: req.body.thumbnail_url,
+      owner_id: req.session.user_id,
+      borrower_id: req.body.borrower_id,
+      available: req.body.available,
+    });
+    res.status(200).json(postData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
 
-// PUT book (update info)
+// PUT book (update status of book and borrower id)
 // localhost:3001/api/books/:id
-router.put("/:id", async (req, res) => {});
+router.put("/:id", async (req, res) => {
+  try {
+    const updateStatus = await Book.update(req.body, {
+      where: { id: req.params.id },
+    });
+    res.status(200).json(updateStatus);
+  } catch (error) {
+    console.error(error);
+    res.status(400).end();
+  }
+});
 
 // DELETE book by id
 // localhost:3001/api/books/:id
