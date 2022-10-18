f06f943330d4febdf8dbcfd08f9539ad55ad56c9
VaishaliQA <patelvaishali1890@gmail.com>
2022-10-17 19:39:29 -0700

    Resloved Library linking issue

--- public/js/browse.js
+++ public/js/browse.js
@@ -1,6 +1,6 @@
 const libraryButton = document.querySelector("#libraryButton");
 
 // Redirect to librarypage
-libraryButton.addEventListener("click", () => {
-  document.location.replace("/library");
-});
+// libraryButton.addEventListener("click", () => {
+//   document.location.replace("/library");
+// });
