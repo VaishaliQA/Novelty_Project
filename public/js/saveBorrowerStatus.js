7ba1d39e8ff4d458f240c77e9c0fb01bcacb007c
isayani <isayani95@gmail.com>
2022-10-19 18:17:31 -0400

    init put route for check out

--- public/js/saveBorrowerStatus.js
+++ public/js/saveBorrowerStatus.js
@@ -1,12 +1,23 @@
-const saveBtn = document.getElementById("statusSave");
+// const saveBtn = document.getElementById("statusSave");
 
-async function saveBorrowerStatus(savedBorrower){
+async function saveBorrowerStatus(event){
+    const borrower_id = document.querySelector("#singleBorrower").value;
+    const bookId = event.target.getAttribute("data-bookId");
+    const response = await fetch(`/api/books/${bookId}`, {
+        method: "PUT",
+        body: JSON.stringify({ borrower_id }),
+        headers: { "Content-Type": "application/json" },
+      });
+
     
     // Event listener on save button
-    saveBtn.addEventListener("click", () => {
-      console.log("on click active");
-      window.alert("Book checked out!");
-    });
+    // saveBtn.addEventListener("click", () => {
+    //   
+    //   window.alert("Book checked out!");
+    // });
 }
+document
+  .querySelector("#statusSave")
+  .addEventListener("click", saveBorrowerStatus);
 
-saveBorrowerStatus()
+// saveBorrowerStatus()
