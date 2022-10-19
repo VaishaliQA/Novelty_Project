aa975868747dadebe465e6018e3990a258c0925c
VaishaliQA <patelvaishali1890@gmail.com>
2022-10-19 10:54:55 -0700

    Throw error message when user entered wrong isbn

--- public/js/add-book.js
+++ public/js/add-book.js
@@ -38,43 +38,47 @@ async function searchBook(isbnInput) {
   )
     .then((response) => response.json())
     .then((data) => {
-      // access first entry of response
-      const bookData = data.items[0];
-      console.log("Book Data:", bookData);
+      // Check if user enter invalid ISBN number
+      if (data.totalItems === 0) {
+        alert("Invalid ISBN. Please enter valid ISBN.");
+      } else {
+        // access first entry of response
+        const bookData = data.items[0];
+        console.log("Book Data:", bookData);
 
-      // parse response
-      const isbnList = bookData.volumeInfo.industryIdentifiers;
-      const title = bookData.volumeInfo.title;
-      const authors = bookData.volumeInfo.authors;
-      const publishedDate = bookData.volumeInfo.publishedDate;
-      const description = bookData.volumeInfo.description; // Show the first 500 characters
-      const categories = bookData.volumeInfo.categories;
-      const thumbnail_url = bookData.volumeInfo.imageLinks.thumbnail;
+        // parse response
+        const isbnList = bookData.volumeInfo.industryIdentifiers;
+        const title = bookData.volumeInfo.title;
+        const authors = bookData.volumeInfo.authors;
+        const publishedDate = bookData.volumeInfo.publishedDate;
+        const description = bookData.volumeInfo.description; // Show the first 500 characters
+        const categories = bookData.volumeInfo.categories;
+        const thumbnail_url = bookData.volumeInfo.imageLinks.thumbnail;
 
-      // grab 13 digit ISBN from array
-      let isbn13 = "";
-      isbnList.forEach((obj) => {
-        if (obj.type === "ISBN_13") {
-          isbn13 = obj.identifier;
-        }
-      });
+        // grab 13 digit ISBN from array
+        let isbn13 = "";
+        isbnList.forEach((obj) => {
+          if (obj.type === "ISBN_13") {
+            isbn13 = obj.identifier;
+          }
+        });
 
-      // structure response into object
-      const obj = {
-        isbn13: isbn13,
-        title: title,
-        authors: authors,
-        published_date: publishedDate,
-        description: description,
-        categories: categories,
-        thumbnail_url: thumbnail_url,
-      };
+        // structure response into object
+        const obj = {
+          isbn13: isbn13,
+          title: title,
+          authors: authors,
+          published_date: publishedDate,
+          description: description,
+          categories: categories,
+          thumbnail_url: thumbnail_url,
+        };
 
-      // Show object on screen
-      const searchedBookInfo = document.getElementById("searched-book-info");
-      //   console.log("Searched Book Info", searchedBookInfo);
-      const bookAddedMessage = document.getElementById("book-added-message");
-      searchedBookInfo.innerHTML = `
+        // Show object on screen
+        const searchedBookInfo = document.getElementById("searched-book-info");
+        //   console.log("Searched Book Info", searchedBookInfo);
+        const bookAddedMessage = document.getElementById("book-added-message");
+        searchedBookInfo.innerHTML = `
         <ul>
             <li class="search-book-result"><span class="search-book-result-title">Title</span>: ${title}</li>
             <li class="search-book-result"><span class="search-book-result-title">Description</span>:
@@ -82,44 +86,45 @@ async function searchBook(isbnInput) {
               0,
               500
             )}<span id ="remaining-description" class="remaining-description">${description.slice(
-        500
-      )}</span>...<span id="read-more" class="read-more">[Read More]</span></li>
+          500
+        )}</span>...<span id="read-more" class="read-more">[Read More]</span></li>
             <li class="search-book-result"><span class="search-book-result-title">Authors</span>: ${authors}</li>
         </ul>
         `;
 
-      // Configure read more link
-      const readMoreLink = document.getElementById("read-more");
-      const remainingDescription = document.getElementById(
-        "remaining-description"
-      );
-      readMoreLink.addEventListener("click", () => {
-        console.log("click");
-        if (remainingDescription.style.display === "inline") {
-          remainingDescription.style.display = "none";
-          readMoreLink.innerHTML = `[Read More]`;
-        } else {
-          remainingDescription.style.display = "inline";
-          readMoreLink.innerHTML = `[Show Less]`;
-        }
-      });
+        // Configure read more link
+        const readMoreLink = document.getElementById("read-more");
+        const remainingDescription = document.getElementById(
+          "remaining-description"
+        );
+        readMoreLink.addEventListener("click", () => {
+          console.log("click");
+          if (remainingDescription.style.display === "inline") {
+            remainingDescription.style.display = "none";
+            readMoreLink.innerHTML = `[Read More]`;
+          } else {
+            remainingDescription.style.display = "inline";
+            readMoreLink.innerHTML = `[Show Less]`;
+          }
+        });
 
-      // Activate Add Book button
-      addBookButton.classList.remove("Disabled");
+        // Activate Add Book button
+        addBookButton.classList.remove("Disabled");
 
-      addBookButton.addEventListener("click", () => {
-        // console.log("Object to Post:", obj);
-        const postRes = postBook(obj)
-          .then(() => {
-            bookAddedMessage.innerHTML = `<p class="book-added-message">Book Added</p>`;
-            return;
-          })
-          .catch(() => {
-            bookAddedMessage.innerHTML = `<p class="book-added-message">Error, Please Try Again/p>`;
-          });
-      });
+        addBookButton.addEventListener("click", () => {
+          // console.log("Object to Post:", obj);
+          const postRes = postBook(obj)
+            .then(() => {
+              bookAddedMessage.innerHTML = `<p class="book-added-message">Book Added</p>`;
+              return;
+            })
+            .catch(() => {
+              bookAddedMessage.innerHTML = `<p class="book-added-message">Error, Please Try Again/p>`;
+            });
+        });
 
-      return;
+        return;
+      }
     })
     .catch((error) => console.log("error", error));
 }
